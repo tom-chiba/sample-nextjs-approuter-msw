91790db6e581f9d3b5e305875e30dfd78553bbe7
TomokiCHIBA <michiamo.t1@gmail.com>
2025-09-13 11:21:53 +0000

    chore: import順を調整する設定追加

--- mocks/setup/browser.ts
+++ mocks/setup/browser.ts
@@ -1,4 +1,4 @@
-import { setupWorker } from "msw/browser";
 import { handlers } from "@/mocks/handlers";
+import { setupWorker } from "msw/browser";
 
 export const worker = setupWorker(...handlers);
